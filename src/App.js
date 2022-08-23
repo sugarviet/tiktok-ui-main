d1293b6b9b119077a9a066eb9b85f7fd30f7a4ad
TrongToann <vietdanghoang1705@gmail.com>
2022-08-23 16:11:22 +0700

    Config Scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import Button from './component/Button';
-import './App.css';
 
+import GlobalStyles from './components/GlobalStyles';
 function App() {
   return (
+    <GlobalStyles>
     <div className="App">
-      <Button />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
+    </GlobalStyles>
   );
 }
 
